30bde718cd318d25389220c47669c394e8229e2d
rohran01 <anthonytrohr@gmail.com>
2016-03-22 16:49:28 -0500

    animates scroll on nav clicks

--- client/client.js
+++ client/client.js
@@ -1,8 +1,62 @@
 var app = angular.module('webApp', []);
 
-app.controller('mainController', ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
-  $scope.scrollTo = function(locationId) {
+app.controller('mainController', ['$scope', '$location', 'anchorSmoothScroll', function($scope, $location, anchorSmoothScroll) {
+  $scope.goTo = function(locationId) {
     $location.hash(locationId);
-    $anchorScroll();
+    anchorSmoothScroll.scrollTo(locationId);
   }
 }])
+
+app.service('anchorSmoothScroll', function(){
+
+    this.scrollTo = function(locationId) {
+
+        // This scrolling function
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+        var startY = currentYPosition();
+        var stopY = elmYPosition(locationId);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 80);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i = startY; i < stopY; i += step) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i = startY; i > stopY; i -= step) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+
+        function elmYPosition(locationId) {
+            var elm = document.getElementById(locationId);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+
+});
