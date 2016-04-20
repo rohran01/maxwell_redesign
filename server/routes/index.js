896dcf5539fb34016602b12955ee8cc61afc2d62
rohran01 <anthonytrohr@gmail.com>
2016-04-20 13:25:24 -0500

    fixes refresh issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,14 @@ var path = require('path');
 
 var router = express.Router();
 
+router.get('/*', function(request, response){
+	response.sendFile(path.join(__dirname, '../public/views/mainPage.html'));
+});
+
 router.get('/', function(request, response) {
   console.log('index.js is working');
   response.sendFile(path.join(__dirname, '../public/views/mainPage.html'));
-})
+});
+
 
 module.exports = router;
