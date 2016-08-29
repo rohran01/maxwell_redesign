c727a944a5b65a2f5d1bf761da4857badd7cc3cb
Anthony Rohr <anthonytrohr@gmail.com>
2016-08-29 12:05:52 -0500

    merge conflicts

--- server/server.js
+++ server/server.js
@@ -8,11 +8,7 @@ app.use(express.static('server/public'));
 
 app.use('/', index);
 
-<<<<<<< HEAD
 var server = app.listen(process.env.PORT || 4000, function() {
-=======
-var server = app.listen(3000, function() {
->>>>>>> 3b3b279a70917c0e7c08bb6d195802479d5e02d7
-  var port = server.address().port;
+var port = server.address().port;
   console.log('listening on port:', port);
-})
+});
