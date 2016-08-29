f6d9bacccccaf1e93a92fde8e62e59589515b128
Anthony Rohr <anthonytrohr@gmail.com>
2016-08-29 11:52:51 -0500

    updates connection string

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.static('server/public'));
 
 app.use('/', index);
 
-var server = app.listen(4000, function() {
+var server = app.listen(process.env.PORT || 4000, function() {
   var port = server.address().port;
   console.log('listening on port:', port);
 })
