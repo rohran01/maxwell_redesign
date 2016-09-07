cc84bf1e9e06c9adf8b7e77a2b507d292383d9e6
rohran01 <anthonytrohr@gmail.com>
2016-09-06 20:56:03 -0500

    updates blue

--- client/client.js
+++ client/client.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
   var commercialActivated = false;
+  var filmActivated = false;
   var commercialToLoad = $('#commercial').children('.work-holder').children();
-  console.log(commercialToLoad.length);
+  var filmToLoad = $('#film').children('.work-holder').children();
 
   var lastId,
       topMenu = $('.scroll-links'),
@@ -20,52 +21,67 @@ $(document).ready(function() {
      var fromTop = $(this).scrollTop() + topMenuHeight;
      var fromBottom = $(this).scrollTop() + $(this).height() - 100;
 
-     console.log('commercial', $('#commercial').offset().top);
-     console.log(fromBottom);
-
-
      // Get id of current scroll item
      var topCur = scrollItems.map(function(){
        if ($(this).offset().top < fromTop)
          return this;
        })
 
-      if (!commercialActivated && $('#commercial').offset().top < fromBottom) {
 
-        $('.video-holder').fadeTo('slow', 1);
+
+      // Get the id of the current element
+      topCur = topCur[topCur.length-1];
+      var id = topCur && topCur.length ? topCur[0].id : "";
+
+      if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems.filter("[href!='#"+id+"']").removeClass('hovered');
+       menuItems.filter("[href='#"+id+"']").addClass('hovered');
+      }
+
+      if (!commercialActivated && $('#commercial').offset().top < fromBottom) {
+        loadVideos(commercialToLoad);
         commercialActivated = true;
       }
 
+      if (!filmActivated && $('#film').offset().top < fromBottom) {
+        loadVideos(filmToLoad);
+        filmActivated = true;
+      }
 
+      if (commercialActivated && id == 'maxwell') {
+        $(commercialToLoad).fadeTo(0, 0);
+        commercialActivated = false;
+      }
 
-    // Get the id of the current element
-    topCur = topCur[topCur.length-1];
-    var id = topCur && topCur.length ? topCur[0].id : "";
+      if (filmActivated && id == 'commercial') {
+        $(filmToLoad).fadeTo(0, 0);
+        filmActivated = false;
+      }
 
-    if (lastId !== id) {
-     lastId = id;
-     // Set/remove active class
-     menuItems.filter("[href!='#"+id+"']").removeClass('hovered');
-     menuItems.filter("[href='#"+id+"']").addClass('hovered');
+    })
+
+    function loadVideos(segment) {
+      console.log('LENGTH', segment.length);
+      for (var i = 0; i < segment.length; i++) {
+        console.log(segment[i]);
+        $(segment[i]).delay(80 * i).fadeTo(600, 1);
+      }
     }
 
-  })
-
-  $('.highlight').on('click', function() {
-    var target = $(this.getAttribute('href'));
-    if ($(this).attr('href') === '#maxwell') {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: $('body').offset().top
-      }, 1000);
-    } else if ($(this).attr('href') !== '#maxwell' && target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: (target.offset().top + 2)
-      }, 1000);
-    }
-  })
-
-
-
+    $('.highlight').on('click', function() {
+      var target = $(this.getAttribute('href'));
+      if ($(this).attr('href') === '#maxwell') {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: $('body').offset().top
+        }, 1000);
+      } else if ($(this).attr('href') !== '#maxwell' && target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: (target.offset().top + 2)
+        }, 1000);
+      }
+    })
 })
